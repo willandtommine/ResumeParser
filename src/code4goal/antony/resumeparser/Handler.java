0b9a1690e33ee9b7d5f1de2131586da8f52f81a0
Will Bradshaw <williebradshaw@Wills-MacBook-Pro.local>
2020-06-30 14:50:05 -0600

    Api first commit

--- src/code4goal/antony/resumeparser/Handler.java
+++ src/code4goal/antony/resumeparser/Handler.java
@@ -1,22 +1,31 @@
 package code4goal.antony.resumeparser;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import java.net.MalformedURLException;
+
 
 import org.json.simple.JSONObject;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.S3Event;
-import com.amazonaws.services.s3.event.S3EventNotification;
 
-import java.net.URL;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+
+
 import com.amazonaws.AmazonServiceException;
 import java.io.InputStream;
-import org.apache.commons.io.FilenameUtils;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import org.apache.commons.codec.binary.Base64;
+
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
@@ -27,69 +36,72 @@ import org.xml.sax.SAXException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
-import com.amazonaws.services.s3.model.S3Object;
 
-import gate.Gate;
 import gate.util.GateException;
 
-public class Handler implements RequestHandler<S3Event, Context> {
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	public Context handleRequest(S3Event s3event, Context context) {
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
 		//String destBktName = "eastbucketwrite";
 
+		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+		
+		
+		byte[] bI = Base64.decodeBase64(event.getBody().getBytes());
+		
+		
 		System.out.println("first line, 2:57");
 
 		File storage = new File("../.././tmp/store.txt");
 		if(storage.exists()) {
-			storage.delete();
 			try {
-				storage.createNewFile();
-			} catch (IOException e) {
+				new PrintWriter(storage).close();
+			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else {
+		}
+		
+		File temp = new File("../.././tmp/temp.txt");
+		if(temp.exists()) {
 			try {
-				storage.createNewFile();
-			} catch (IOException e) {
+				new PrintWriter(temp).close();
+			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		
 		
+		try {
+			 
+			OutputStream os = new FileOutputStream(storage);
+            os.write(bI);
+            System.out.println("Write bytes to file.");
+            
+            os.close();
+        } catch (Exception e) {
+        	
+            e.printStackTrace();
+        }
 
+		
 
-		S3EventNotification.S3EventNotificationRecord record = (s3event).getRecords().get(0);
-
-		String bucket_name = record.getS3().getBucket().getName();
-		String key_name = record.getS3().getObject().getUrlDecodedKey();
+		
 		
 
 		final AmazonS3Client s3 = (AmazonS3Client) AmazonS3ClientBuilder.standard().build();
 
 		try {
 
-			S3Object o = s3.getObject(bucket_name, key_name);
+		
 
-			String ext = FilenameUtils.getExtension(key_name);
-			String outputFileFormat = "";
-
-			if (ext.equalsIgnoreCase("html") | ext.equalsIgnoreCase("pdf") | ext.equalsIgnoreCase("doc")
-					| ext.equalsIgnoreCase("docx")) {
-				outputFileFormat = ".html";
-				// handler = new ToXMLContentHandler();
-			} else if (ext.equalsIgnoreCase("txt") | ext.equalsIgnoreCase("rtf")) {
-				outputFileFormat = ".txt";
-			} else {
-				System.out.println("Input format of the file " + key_name + " is not supported.");
-				return null;
-			}
+		
 
 			
 			ContentHandler handler = new ToXMLContentHandler();
 
-			InputStream stream = o.getObjectContent();
+			InputStream stream = new FileInputStream(storage);
 			//String url = "https://" + destBktName + ".s3.amazonaws.com/" + OUTPUT_FILE_NAME;
 
 			AutoDetectParser parser = new AutoDetectParser();
@@ -97,11 +109,11 @@ public class Handler implements RequestHandler<S3Event, Context> {
 			try {
 
 				parser.parse(stream, handler, metadata);
-				FileWriter htmlFileWriter = new FileWriter(storage);
+				FileWriter htmlFileWriter = new FileWriter(temp);
 				htmlFileWriter.write(handler.toString());
 				htmlFileWriter.flush();
 				htmlFileWriter.close();
-				
+				stream.close();
 
 				// s3.putObject(destBktName, OUTPUT_FILE_NAME, handler.toString());
 			} catch (IOException e) {
@@ -125,17 +137,22 @@ public class Handler implements RequestHandler<S3Event, Context> {
 		} catch (AmazonServiceException e) {
 			System.err.println(e.getErrorMessage());
 			System.exit(1);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 		
 
 		try {
 
 			
-			JSONObject parsedJSON = ResumeParserProgram.loadGateAndAnnie(storage);
+			JSONObject parsedJSON = ResumeParserProgram.loadGateAndAnnie(temp);
 			System.out.println("just got some parsed Json");
 			System.out.println(parsedJSON.toJSONString());
 			
-			return null;
+			response.setBody(parsedJSON.toJSONString());
+			System.gc();
+			return response;
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -144,8 +161,8 @@ public class Handler implements RequestHandler<S3Event, Context> {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		return null;
+		System.gc();
+		return response;
 
 	}
 
